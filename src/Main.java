5b63b859c30e8b42ff2ba8f1a536122b6e20e30a
tiago <TiagoPadrao20>
2025-11-14 20:12:47 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Aprog Test ");
+
+        int[][] moodMap = new int[5][12];
+        getMoodMap(moodMap);
+
+    }
+
+    private static void getMoodMap(int[][] array) {
+        System.out.println("Mood Map received");
+        for (int i = 0; i < array.length; i++) {
+        }
+    }
+}
