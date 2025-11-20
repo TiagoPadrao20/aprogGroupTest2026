56dd5ee7ce09078e9b30c1971f773f147ad06b35
tiago <TiagoPadrao20>
2025-11-20 18:57:22 +0000

    change the method of getInputFromFile

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,47 @@ public class Main {
         File file = new File("/Users/tiagopadrao/desktop/uni/prcmp/fileTest");
         int[][] moodMap = getInputFromFile(file);
 
-        //aqui, chamamos o metodo para fazer print apenas do header, passamos o tamanho do 1~array, que seria o array dos dias
         printDaysHeader(moodMap[0].length);
-
         printMoodMapWithPeople(moodMap);
     }
 
-    //metodo que recebemosu m ficheiro e convertemos para uma array
+
+    private static void readFromFile(File file, String[] arr) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        int index = 0;
+
+        while (scanner.hasNext() && index < arr.length) {
+            String word = scanner.next();
+            arr[index] = word;
+            index++;
+        }
+    }
+
     private static int[][] getInputFromFile(File file) throws FileNotFoundException {
-        Scanner sc = new Scanner(file);
+        Scanner scanner = new Scanner(file);
 
-        //para ignorar a primeira linha do ficheiro
-        if (sc.hasNextLine()) sc.nextLine();
+        if (scanner.hasNextLine()) scanner.nextLine();
 
-        String line = sc.nextLine();
-        //aqui criei um array de dimensões, com o conteúdo que esta na line. (dias e quantidade de pessoas)
-        String[] dimensions = line.split(" ");
-
+        String lineWithDimensions = scanner.nextLine();
+        String[] dimensions = lineWithDimensions.split(" ");
 
         int quantityOfPeople = Integer.parseInt(dimensions[0]);
         int quantityOfDays = Integer.parseInt(dimensions[1]);
 
         int[][] moodMap = new int[quantityOfPeople][quantityOfDays];
 
-        for (int row = 0; row < quantityOfPeople; row++) {
-            //pega todas as linhas do ficheiro, e a cada iteração, guarda num array temporario o conteudo
-            String rowLine = sc.nextLine();
-            String[] tempArrayOfLines = rowLine.split(" ");
-            System.out.println(tempArrayOfLines[0]);
-            for (int col = 0; col < quantityOfDays; col++) {
-                moodMap[row][col] = Integer.parseInt(tempArrayOfLines[col]);
+        int people = 0;
+        int days = 0;
+
+        while (scanner.hasNext() && people < moodMap.length) {
+            moodMap[people][days] = Integer.parseInt(scanner.next());
+            days++;
+            if (days == moodMap[people].length) {
+                days = 0;
+                people++;
             }
         }
-
-        sc.close();
+        scanner.close();
         return moodMap;
     }
 
